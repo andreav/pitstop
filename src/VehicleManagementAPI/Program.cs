b6d120526b3d1b42d3d886f79635f3c3f43cf325
valle <valle@is-mb.it>
2022-01-25 12:31:03 +0100

    added exaple code for generating swagger..json from command line

--- src/VehicleManagementAPI/Program.cs
+++ src/VehicleManagementAPI/Program.cs
@@ -2,6 +2,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore;
 using Serilog;
+using System;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi.Extensions;
+using Microsoft.OpenApi;
 
 namespace Pitstop.Application.VehicleManagement
 {
@@ -15,11 +20,28 @@ namespace Pitstop.Application.VehicleManagement
 
         private static IWebHost BuildWebHost(string[] args)
         {
-            return WebHost.CreateDefaultBuilder(args)
+            var webHost = WebHost.CreateDefaultBuilder(args)
                 .UseSerilog()
                 .UseHealthChecks("/hc")
                 .UseStartup<Startup>()
                 .Build();
+
+            // If args, then it's for generating swagger and exit
+            // otherwise run web server
+            if (args.Length > 0)
+            {
+                Console.WriteLine(GenerateSwagger(webHost, args[0]));
+                Environment.Exit(0);
+            }
+
+            return webHost;
+        }
+
+        private static string GenerateSwagger(IWebHost host, string docName)
+        {
+            ISwaggerProvider sw = (ISwaggerProvider)host.Services.GetService(typeof(ISwaggerProvider));
+            OpenApiDocument doc = sw.GetSwagger(docName);
+            return doc.SerializeAsJson(OpenApiSpecVersion.OpenApi3_0);
         }
     }
 }
